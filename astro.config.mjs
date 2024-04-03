7fa433663cfa868d69f4e5917ec952a757913658
erasebegin <chris.james.haupt@gmail.com>
2024-04-03 19:03:09 +0100

    attempt to fix path issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://erasebegin.github.io",
-  base: "my-notes",
+  base: "/my-notes",
   integrations: [mdx(), sitemap()],
 });
