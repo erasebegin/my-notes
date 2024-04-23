c51703eed07fcb9c86124ec458a3fb30b62fda62
erasebegin <chris.james.haupt@gmail.com>
2024-04-23 17:42:18 +0100

    bit of a restructure

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://erasebegin.github.io",
-  base: "/my-notes",
+  site: "https://boatsofnotes.netlify.app",
   integrations: [mdx(), sitemap()],
 });
